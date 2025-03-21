d18e209f0748952bcc939a053110ea234e1d3798
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-03-22 01:39:13 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -4,8 +4,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div>
+    <div className="bg-[url('/space.jpeg')]  bg-cover bg-center h-screen text-white">
+      <div >
         <Navbar/>
+      </div>
+      <div className="flex flex-col gap-1.5 text-6xl w-150 pl-8 mt-35">
+        <h2>Coming Soon:</h2>
+        <h2>DAKSphere-Your Goals</h2>
+      </div>
+      <div className="flex flex-col gap-3 mt-4 ml-1.5">
+        <p className="w-150 pl-8 text-[#87898F]">Experience the future of goal-setting and achievement with DAKSphere, a revolutionary platform that empowers you to reach new heights</p>
+        <div className=" flex ml-8 gap-8 mt-4.5">
+          <button className="bg-[#E3C7E0] w-45 p-1.5 rounded-3xl text-[#0b072a]">Explore Now</button>
+          <button className="bg-transparent border-1 w-45 rounded-3xl p-1.5">Join the Journey</button>
+        </div>
+      </div>
+      </div>
+      <div>
+        heasl
       </div>
     </>
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,11 @@
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between p-2.5">
+    <div className="flex flex-row justify-between p-2.5 ml-2">
         <div>
             <p>DAKSphere</p>
         </div>
-        <ul className="flex gap-12 ">
+        <ul className="flex gap-12 mr-2 ">
             <li>Home</li>
             <li>Services</li>
             <li>About Us</li>
